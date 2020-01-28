e04423e8e3ea289714130d04d7c9db57d2ff626e
Hor123 <hortensere@aol.com>
2020-01-28 13:28:59 +0100

    Ex PHP Ajax Json

---
+++ README.md
@@ -0,0 +1,7 @@
+Consignes :
+
+1 - Importez le fichier sql dans une base de données.
+2 - Ajoutez dans la table product_purchased de nouvelles entrées
+3 - En adaptant le code existant, afficher lors du click sur le lien "afficher les achats", 
+les produits achetés.
+
---
+++ ajax.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sstienface
+ * Date: 25/01/2019
+ * Time: 11:17
+ */
+
+
+//Remplacer les valeurs si besoin
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+// $dbname = "simon";
+$dbname = "bdd";
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+// Selectionner la base à utiliser $conn->select_db($dbname);
+    $conn->select_db($dbname);
+
+
+
+    switch($_GET['action'])
+    {
+        case"affProducts":
+            $res = "select * from `products` where 1";
+            break;
+        case"affPurchased":
+            $achats = "select * from `products_purchased` where 1";
+            break;
+    }
+
+    if(isset($res))
+    {
+
+        $arr = array();
+        $result = $conn->query($res);
+
+
+        while($data = $result->fetch_assoc())
+        {
+
+            $arr[] = $data;
+
+        }
+
+        echo json_encode($arr);
+
+
+    }
+
+    if(isset($achats))
+    {
+
+        $arr1 = array();
+        $result1 = $conn->query($achats);
+
+
+        while($data1 = $result1->fetch_assoc())
+        {
+
+            $arr1[] = $data1;
+
+        }
+
+        echo json_encode($arr1);
+
+
+    }
+
+
+}
+
+
